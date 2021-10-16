74b4fe5448277a96b4cb4cfd6bde289c87a334f4
Rajeshkumar Sivaprakasam <rajesh.kumar@goavega.com>
2021-10-16 13:53:57 +0530

    Chart-engine added with General File structure!

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import { ChatEngine } from 'react-chat-engine';
 import './App.css';
-
+import ChatFeed from './components/ChatFeed'
 const App = () =>{
     return(
         <ChatEngine 
             height='100vh'
-            projectID=''
-            userName=''
-            userSecret=''
+            projectID='a8085311-877e-44bf-8a03-faf1f944dcb2'
+            userName='infothamizha'
+            userSecret='123456'
+            renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
         />
     );
 }
---
+++ src/components/ChatFeed.jsx
@@ -0,0 +1,14 @@
+import MessageForm from "./MessageForm";
+import MyMessage from "./MyMessage";
+import TheirMessage from "./TheirMessage";
+
+const ChatFeed = (props) =>{
+    { chats, activeChat, userName, messages } = props;
+    return(
+        <div>
+            Chat Feed!
+        </div>
+    )
+}
+
+export default ChatFeed;
